42979b90453ee3f433c5e353f1c6b851b32d110f
Giacomo Frascarelli <frascarelli.1917888@studenti.uniroma1.it>
2024-12-06 18:45:33 +0100

    first commit

---
+++ src/Camera3D.h
@@ -0,0 +1,35 @@
+#ifndef _CAMERA3D_H_
+#define _CAMERA3D_H_
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
+
+class Camera3D {
+public:
+	Camera3D(glm::vec3 cameraPos, glm::vec3 cameraFront);
+
+	void setPos(glm::vec3 newPosition) { cameraPos = newPosition; needsUpdating = true; }
+	void setDirection(glm::vec3 newDirection) { cameraFront = newDirection; needsUpdating = true; }
+
+	void update();
+
+	const glm::mat4& getCameraMatrix() const { return cameraMatrix; }
+
+	const glm::vec3& getCameraPos() const { return cameraPos; }
+	const glm::vec3& getCameraFront() const { return cameraFront; }
+
+    float pitch;
+    float yaw = -90.0f;
+private:
+
+	glm::mat4 cameraMatrix;
+
+	
+	glm::vec3 cameraPos;
+	glm::vec3 cameraFront;
+
+	bool needsUpdating;
+};
+
+#endif
